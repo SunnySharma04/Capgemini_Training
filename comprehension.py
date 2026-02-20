ac78731a6e1707a127df579d2863a5135f16687c
Sunny Sharma <sunnysharma112004@gmail.com>
2026-02-20 23:12:55 +0530

    Remove unnecessary practice and log files

--- comprehension.py
+++
@@ -1,7 +0,0 @@
-inp = "Python is very very easy language"
-out = [(word.lower(), len(word)) for word in inp.split()]
-print(out)
-
-inp1 = "Hello WorLD"
-out1 = {char: ord(char) for char in inp1 if char.isupper()}
-print(out1)
