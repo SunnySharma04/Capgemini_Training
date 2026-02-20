f0c3a067a5027112013f027f63f9d815c1aa81ee
Sunny Sharma <sunnysharma112004@gmail.com>
2026-02-20 22:35:40 +0530

    Day 1 - Python Variables Implementation

---
+++ Day1_variables.py
@@ -0,0 +1,13 @@
+# import keyword
+# # help("keywords")
+# print(keyword.kwlist)
+# print(len(keyword.kwlist))
+
+a = 10
+print(id(10))
+print(id(a))
+b = 256
+print(id(b))
+c = 257
+print(id(c))
+print(id(c))
